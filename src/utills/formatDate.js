e6734016ea4b2f4d3fdec0cdba2544b6fdb466f6
Aulia Azzahra <auliazzahraa28@gmail.com>
2025-12-16 17:00:30 +0700

    hapus file yang tidak terpakai

--- src/utills/formatDate.js
+++
@@ -1,9 +0,0 @@
-// src/utils/formatDate.js
-export function formatTanggal(isoString) {
-  const tanggal = new Date(isoString);
-  return tanggal.toLocaleDateString("id-ID", {
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric",
-  });
-}
